b10703af80f5f51315e52ce25ac5aacc3d7fcce0
call-fold <568925701@qq.com>
2016-04-16 02:26:31 +0800

    add markdown

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -7,8 +7,7 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', 'article.views.home'),
-                       url(r'^(?P<my_args>\d+)/$',
+                       url(r'^$', 'article.views.home', name='home'),
+                       url(r'^(?P<id>\d+)/$',
                            'article.views.detail', name='detail'),
-                       url(r'^test/$', 'article.views.home'),
                        )
