8b2809a18da6189c2d3a937aefed943862f97e24
call-fold <568925701@qq.com>
2016-04-24 23:16:11 +0800

    add post title

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'j#ow!1t2n7r5+_f-*yl4&&rr4)yik4rwj^4#jyc75v7njy^h!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
