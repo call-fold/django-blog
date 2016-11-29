fbb0c894c6d2c44887bdebd66f0bea752c1a2451
Su Lingfeng <568925701@qq.com>
2016-11-30 03:04:33 +0800

    solve nginx 504 bug

--- MovieCrawler/Common/CommonMovieCrawler.py
+++ MovieCrawler/Common/CommonMovieCrawler.py
@@ -11,16 +11,16 @@ def get_movie_download_tuple(url, decode_type='utf-8'):
 
 def get_movie_download_link(url, decode_type='utf-8'):
     download_link_list = get_links_from_html_keyword(url, 'ftp', decode_type)
-    # print(url)
-    return download_link_list[0]
+    return download_link_list
 
 
 def get_movie_download_list(real_movie_link_list, decode_type='utf-8', if_add_title=True):
     movie_download_list = []
     for real_movie_link in real_movie_link_list:
         if if_add_title:
-            movie_download = get_movie_download_tuple(real_movie_link, decode_type)
+            movie_download_link_by_page = get_movie_download_tuple(real_movie_link, decode_type)
+            movie_download_list.append(movie_download_link_by_page)
         else:
-            movie_download = get_movie_download_link(real_movie_link, decode_type)
-        movie_download_list.append(movie_download)
+            movie_download_list_by_page = get_movie_download_link(real_movie_link, decode_type)
+            movie_download_list.extend(movie_download_list_by_page)
     return movie_download_list
