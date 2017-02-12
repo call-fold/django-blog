4c078abebb85ee02d1ee7132861b3d4b873c5536
Su Lingfeng <568925701@qq.com>
2017-02-13 00:41:32 +0800

    get crawled movies from redis

--- article/views.py
+++ article/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from movie_crawler.movie_search import get_search_url, get_total_movie_download_list
+from movie_crawler.movie_search_from_redis import get_movie_list
 from article.models import Article
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -90,9 +90,7 @@ def movie_input_post(request):
 
 def ajax_list(request):
     input_name = input_movie_name
-    my_search_index_url = get_search_url('http://s.dydytt.net/plus/search.php?kwtype=0&searchtype=title&keyword=',
-                                         input_name)
-    search_movie_download_list = get_total_movie_download_list(my_search_index_url, 'gbk', False)
+    search_movie_download_list = get_movie_list(input_name)
     out_list = []
     if len(search_movie_download_list) != 0:
         for download_link in search_movie_download_list:
--- movie_crawler/movie_search_from_redis.py
+++ movie_crawler/movie_search_from_redis.py
@@ -12,16 +12,19 @@ def get_movie_list(pattern):
     pattern = str(pattern).replace('\\x', '%')
     pattern = re.findall(r'b\'(.+?)\'', pattern)[0]
     list_title = strict_redis.keys(pattern)
+    movie_list = []
     for title in list_title:
-        movie_list = strict_redis.smembers(title)
-        print(title + ' :')
-        for movie in movie_list:
-            print(movie)
+        movie_set = strict_redis.smembers(title)
+        for movie in movie_set:
+            movie_list.append(movie)
+    return movie_list
 
 
 def main():
     movie_title = '海贼王'
-    get_movie_list(movie_title)
+    movie_list = get_movie_list(movie_title)
+    for movie in movie_list:
+        print(movie)
 
 
 if __name__ == '__main__':
