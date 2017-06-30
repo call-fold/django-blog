be6d8f011c784bd3e3602d0b98366590ac561526
call-fold <slf568925701@gmail.com>
2017-07-01 01:35:30 +0800

    change movie search url in view.py

--- article/views.py
+++ article/views.py
@@ -79,7 +79,7 @@ def ajax_list(request):
             out_list.append('\n')
     else:
         # 数据库中没有, 直接去网上找
-        my_search_index_url = get_search_url('http://s.dydytt.net/plus/search.php?kwtype=0&searchtype=title&keyword=',
+        my_search_index_url = get_search_url('http://s.dydytt.net/plus/so.php?kwtype=0&searchtype=title&keyword=',
                                              input_name)
         search_movie_download_list = get_total_movie_download_list(my_search_index_url, 'gbk', False)
         if len(search_movie_download_list) != 0:
